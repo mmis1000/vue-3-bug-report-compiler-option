9a2134f78234232847595acd5dba878cde5bf321
Mmis1000 <mmis1000@yahoo.com.tw>
2020-07-30 15:08:21 +0800

    Add all

---
+++ vue.config.js
@@ -0,0 +1,18 @@
+const vuePluginNext = require.resolve('vue-cli-plugin-vue-next')
+const vueLoaderNext = require.resolve('vue-loader', {paths: [vuePluginNext] })
+
+module.exports = {
+    chainWebpack: config => {
+        config.module
+            .rule('vue')
+            .use('vue-loader')
+            .loader(vueLoaderNext)
+            .tap(options => {
+                // modify the options...
+                return {
+                    ...options,
+                    compiler: require('./compiler')
+                }
+            })
+    }
+}
